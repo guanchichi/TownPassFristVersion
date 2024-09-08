5d44b3314c2423aa4627eda2c0ad2d55e2fc9cdf
chichi <guanshaoqi13@gmail.com>
2024-09-08 08:02:40 +0800

    first commits

---
+++ lib/myWidget.dart
@@ -0,0 +1,4 @@
+
+String chenPinHao = 'https://187c-2401-e180-8d05-5393-f195-2fc6-efd6-878c.ngrok-free.app/';
+String chenPinHaoIcon = 'assets/svg/icon_talk.svg';
+String chenPinHaoTitle = '場地快租';
---
+++ lib/page/city_service/model/trending_service_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:town_pass/gen/assets.gen.dart';
+import '../../../myWidget.dart';
+abstract final class TrendingServiceModel {
+  static List<TrendingService> get serviceList {
+    return [
+      TrendingService(
+        title: '找地點',
+        icon: Assets.svg.iconLocationSearch.svg(),
+        url: 'https://taipei-pass-service.vercel.app/surrounding-service/',
+      ),
+      TrendingService(
+        icon: Assets.svg.iconDashboardReports.svg(),
+        title: '市民儀表板',
+        url: 'https://dashboard.gov.taipei/',
+      ),
+      TrendingService(
+        icon: Assets.svg.iconLocationSearch.svg(),
+        title: '施工資訊',
+        url: '',
+      ),
+      TrendingService(
+        icon: Assets.svg.b.svg(),
+        title: chenPinHaoTitle,
+        url: chenPinHao,
+      ),
+      // 在此列表後加入新熱門按鈕
+      // add new trending service button here
+    ];
+  }
+}
+
+class TrendingService {
+  final Widget icon;
+  final String title;
+  final String url;
+
+  const TrendingService({
+    required this.icon,
+    required this.title,
+    required this.url,
+  });
+}
